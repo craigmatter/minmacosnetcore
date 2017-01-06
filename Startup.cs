cb9b683fce6cd550d424bd27dcae6aae4e34acca
Craig Ferndale <craigferndale@S13K-MBP.local>
2017-01-06 20:36:02 +0200

    Refactored Startup to use routes

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace ConsoleApplication
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore()
+                    .AddJsonFormatters(options => options.ContractResolver = new CamelCasePropertyNamesContractResolver());
+        }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {            
-            app.Run(async (context) => await context.Response.WriteAsync("{'test':'passed'}"));
+            app.UseMvc();
         }
     }
 }
